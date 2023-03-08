0b8625491c137488a471f68f5674fef5ee59c591
jmedina <ndgns2@gmail.com>
2023-03-08 13:51:13 -0700

    Add mongoDB dependencies

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+require('dotenv').config()
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log('Connected to database'));
+
+
 app.use(express.static("public"));
 app.use(cors());
 app.use(
@@ -684,7 +693,7 @@ app.post("/api/inventory", async (req, res) => {
     code: code || "N/A",
     user: user,
     id: id,
-    price: price
+    price: price,
   };
   app.locals.inventory.push(newPost);
 
