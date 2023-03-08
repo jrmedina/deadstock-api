e8cd5393ce377ba565956e991f305db256c30a93
jmedina <ndgns2@gmail.com>
2023-03-08 14:28:57 -0700

    Connect user request to db

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const router = express.Router()
-
-
+const express = require("express");
+const router = express.Router();
+const user = require("../models/user");
 
 // GET ALL
-router.get('/', (req, res) => {
-
-})
+router.get("/", async (req, res) => {
+  try {
+    const users = await user.find();
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 // GET ONE
 router.get("/:id", (req, res) => {
-   
+  res.send(req.params.id);
 });
 // CREATING ONE
-router.post("/", (req, res) => {
-
-});
+router.post("/", (req, res) => {});
 // UPDATING ONE
-router.patch("/:id", (req, res) => {
-
-});
+router.patch("/:id", (req, res) => {});
 
 // DELETE ONE
 
 router.delete("/:id", (req, res) => {});
 
-
-
-module.exports = router
+module.exports = router;
